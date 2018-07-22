9da728aed2b2bf3a17e0c0cb58e9db757be21c95
José Hurtado <jhurtado@belatrixsf.com>
2018-07-21 23:13:16 -0500

    update remote driver lib and geckodriver

--- src/test/java/pe/com/automation/exam/fw/ParentScenario.java
+++ src/test/java/pe/com/automation/exam/fw/ParentScenario.java
@@ -5,6 +5,7 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.Point;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 import pe.com.automation.exam.page.LandingPage;
 
@@ -16,21 +17,32 @@ public class ParentScenario {
 	public void startChrome() {
 
 		try {
-			driver = new ChromeDriver();
+			//driver = new ChromeDriver();
+			System.setProperty("webdriver.gecko.driver", "src/test/resources/driver/geckodriver");
+			driver = new FirefoxDriver();
 			driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
-	        driver.manage().window().setPosition(new Point(0, 0));	
-	        driver.manage().window().maximize();
-			
+			driver.manage().window().setPosition(new Point(0, 0));
+			driver.manage().window().maximize();
+
 		} catch (Exception e) {
 			System.out.println("Exception when registering the Driver " + e);
 		}
-		
+
 		landingPage = new LandingPage(driver);
 
 	}
 
 	protected void closeDriver() {
-		driver.quit();
-	}
+		if (driver != null) {
+			try {
+				
+				driver.close();
+				driver.quit();
 
+			} catch (Exception e) {
+				throw new RuntimeException(e);
+			}
+		}
+
+	}
 }
